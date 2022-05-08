f4d7d521c82d6293d04c3868ac8b41f165187d53
Sanket Bodake <sanketbodake451998@gmail.com>
2022-05-08 20:33:17 +0530

    add file

---
+++ app.py
@@ -0,0 +1,45 @@
+# from crypt import methopythonds
+from flask import Flask,render_template,request
+import pickle
+import pandas as pd
+import artifacts
+application=Flask(__name__)
+
+
+normal_model=pickle.load(open(r'E:\10.python\project\advertiment_prediction\advertiment-subscription-prediction\Normal.pkl',"rb"))  # Normal Scaling Apply model load
+knn_model=pickle.load(open(r'E:\10.python\project\advertiment_prediction\advertiment-subscription-prediction\KNN_model.pkl',"rb"))
+@application.route("/")
+def home():
+    
+    return render_template("index.html")
+
+@application.route("/Predict",methods=["POST","GET"])
+def adv_prediction():
+    try:
+        Gender=request.form.get("Gender")
+        Age=int(request.form.get("Age"))
+        Estimated_Salary=int(request.form.get("Estimated_Salary"))
+        dict1={"Gender":[Gender],"Age":[Age],"Estimated_Salary":[Estimated_Salary]}
+        df=pd.DataFrame(dict1)
+        df['Gender'].replace({"Male":1,"Female":0},inplace=True)
+        print(df)
+        
+        df[df.columns]=normal_model.transform(df)
+        result=knn_model.predict(df)   
+        for i in result:
+            if int(i)==0:
+                final_result1= "User May not be Subscribe"
+                return  render_template("index.html",Prediction1=final_result1)
+            else:
+                final_result2="User May be Subscribe"
+                return  render_template("index.html",Prediction2=final_result2)
+    except:
+        final_result3= "Enter Valid Response"
+        return  render_template("index.html",Prediction3=final_result3)
+        
+
+
+
+
+if __name__=="__main__":
+    application.run(debug=True,host="0.0.0.0", port=8080)
